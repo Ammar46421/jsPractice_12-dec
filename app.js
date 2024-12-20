e37a35d6e7cab253d56249652b371e5a01505771
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-12-20 18:32:14 +0500

    update

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// method 1
+// import func, { divide, subtract, multiply } from './script.js';
+
+// let div = divide(5, 2);
+// let plus = func(3,9);
+// let minus = subtract(5, 3);
+// let mult = multiply(4, 3);
+
+// divide()
+
+
+// console.log(div); 
+// console.log(plus); 
+// console.log(minus); 
+// console.log(mult); 
+
+
+// method 2
+
 import  * as script from './script.js';
 
 let div = script.divide(5, 2);
@@ -5,15 +24,9 @@ let plus = script.add(3,9);
 let minus = script.subtract(5, 3);
 let mult = script.multiply(4, 3);
 
-// divide()
 
 
 console.log(div); 
 console.log(plus); 
 console.log(minus); 
 console.log(mult); 
-
-
-// import { divide, subtract, multiply } from './script.js';
-
-// let div = divide(5, 2);
--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// method 1
+
+// const divide = (a, b)=> a/b;
+
+// const multiply = (a, b)=> a*b;
+
+// const subtract = (a, b)=> a-b;
+
+// const add = (a, b)=> a+b;
+
+// export default add;
+
+// export {
+//     divide,
+//     multiply,
+//     subtract,
+// }
+
+
+// method 2
+
 const divide = (a, b)=> a/b;
 
 const multiply = (a, b)=> a*b;
