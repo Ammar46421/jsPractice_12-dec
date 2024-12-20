82a3cdff8a9ad9491aaecf56c2c363001911156b
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-12-20 17:52:40 +0500

    update

---
+++ app.js
@@ -0,0 +1,11 @@
+import * as script from './script.js';
+
+let div = script.divide(5,2);
+let plus = script.add(3,9);
+let minus = script.subtract(5,3);
+let mult = script.multiply(4,3);
+
+// divide()
+
+
+console.log(div, plus, minus, mult); 
---
+++ script.js
@@ -0,0 +1,14 @@
+const divide = (a, b)=> a/b;
+
+const multiply = (a, b)=> a*b;
+
+const subtract = (a, b)=> a-b;
+
+const add = (a, b)=> a+b;
+
+export {
+    divide,
+    multiply,
+    subtract,
+    add,
+}
